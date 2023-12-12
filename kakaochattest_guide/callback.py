8026d089ccc40e9038841149af6b4c6b96d26f9d
bactoria(황준오) <bactoria.dev@kakaocorp.com>
2023-12-12 18:05:30 +0900

    랭체인 프레임워크 적용

--- kakaochattest_guide/callback.py
+++ kakaochattest_guide/callback.py
@@ -3,32 +3,38 @@ import os
 import time
 
 import aiohttp
-import openai
 from dotenv import load_dotenv
+from langchain.chains import LLMChain
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import SystemMessage
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    HumanMessagePromptTemplate,
+)
 
 from dto import ChatbotRequest
 
 load_dotenv()
-openai.api_key = os.environ.get('API_KEY')
+os.environ["OPENAI_API_KEY"] = os.environ.get('API_KEY')
 
 SYSTEM_MSG = "당신은 카카오 서비스 제공자입니다."
 logger = logging.getLogger("Callback")
 
 
 async def callback_handler(request: ChatbotRequest) -> dict:
-    # ===================== start =================================
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": SYSTEM_MSG},
-            {"role": "user", "content": request.userRequest.utterance},
-        ],
-        temperature=0,
-    )
-    # focus
-    output_text = response.choices[0].message.content
+    imput_text = request.userRequest.utterance
 
-    # 참고링크 통해 payload 구조 확인 가능
+    llm = ChatOpenAI(temperature=0.1, max_tokens=500, model='gpt-3.5-turbo')
+
+    system_message_prompt = SystemMessage(content='assistant는 user 가 카카오싱크에 대해 궁금한 부분을 천천히 친절하게 대답해줘.')
+    human = HumanMessagePromptTemplate.from_template('{text}\n---\n 에 대한 답변을 친절하게 대답해줘.')
+    prompt = ChatPromptTemplate.from_messages([system_message_prompt, human])
+
+    chain = LLMChain(llm=llm, prompt=prompt)
+
+    output_text = chain.run(text = imput_text)
+    print("답변: " + output_text)
+
     payload = {
         "version": "2.0",
         "template": {
