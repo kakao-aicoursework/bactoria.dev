3d331571531236e09824a6cde8cbf0fa8c80f27c
bactoria(황준오) <bactoria.dev@kakaocorp.com>
2023-12-15 13:07:16 +0900

    vector DB 세팅

--- kakaochattest_guide/callback.py
+++ kakaochattest_guide/callback.py
@@ -20,12 +20,18 @@ os.environ["OPENAI_API_KEY"] = os.environ.get('API_KEY')
 SYSTEM_MSG = "당신은 카카오 서비스 제공자입니다."
 logger = logging.getLogger("Callback")
 
+import db.db
+
+db.db.upload_kakao_social_data()
+db.db.upload_kakao_sink_data()
+db.db.upload_kakaotalk_channel_data()
 
 def get_prompt(filename):
     with open("prompt/" + filename, "r") as fin:
         return fin.read()
 
-llm = ChatOpenAI(temperature=0.1, max_tokens=500, model='gpt-3.5-turbo')
+
+llm = ChatOpenAI(temperature=0.1, model='gpt-3.5-turbo-16k')
 system_message_prompt = SystemMessage(content='assistant는 user 가 카카오싱크에 대해 궁금한 부분을 천천히 친절하게 대답해줘.')
 human = HumanMessagePromptTemplate.from_template('{text}\n---\n 에 대한 답변을 친절하게 대답해줘.')
 prompt = ChatPromptTemplate.from_messages([system_message_prompt, human])
@@ -34,6 +40,7 @@ INTENT_PROMPT = get_prompt("intent_prompt.txt")
 FIND_INTENT_CHAIN = LLMChain(llm=llm, prompt=ChatPromptTemplate.from_messages([INTENT_PROMPT]), verbose=True)
 chain = LLMChain(llm=llm, prompt=prompt, verbose=True)
 
+
 async def callback_handler(request: ChatbotRequest) -> dict:
     input_text = request.userRequest.utterance
 
@@ -41,15 +48,16 @@ async def callback_handler(request: ChatbotRequest) -> dict:
     logger.info("intent: " + intent)
 
     if intent == "kakao_social":
-        output_text = "인텐트는 kakao_social"
+        related_docs = db.db.query_on_kakao_social(input_text)
     elif intent == "kakao_sink":
-        output_text = "인텐트는 kakao_sink"
+        related_docs = db.db.query_on_kakao_sink(input_text)
     elif intent == "kakaotalk_channel":
-        output_text = "인텐트는 kakaotalk_channel"
+        related_docs = db.db.query_on_kakaotalk_channel(input_text)
     else:
         output_text = "대답할 수 없어요."
 
-    print("답변: " + output_text)
+    print(f"related_docs: {related_docs}")
+    output_text = related_docs
 
     payload = {
         "version": "2.0",
---
+++ kakaochattest_guide/db/db.py
@@ -0,0 +1,106 @@
+import json
+
+import chromadb
+from chromadb.api.models import Collection
+from langchain_community.document_loaders import TextLoader
+
+CHROMA_PERSIST_PATH = "chroma"
+KAKAO_SOCIAL_COLLECTION_NAME = "kakao-social-collection"
+KAKAO_SINK_COLLECTION_NAME = "kakao-sink-collection"
+KAKAOTALK_CHANNEL_COLLECTION_NAME = "kakaotalk-channel-collection"
+
+
+def _createCollection(collection_name: str):
+    return chromadb.PersistentClient().get_or_create_collection(
+        name=collection_name,
+        metadata={"hnsw:space": "cosine"}
+    )
+
+
+kakao_social_collection = _createCollection(KAKAO_SOCIAL_COLLECTION_NAME)
+kakao_sink_collection = _createCollection(KAKAO_SINK_COLLECTION_NAME)
+kakaotalk_channel_collection = _createCollection(KAKAOTALK_CHANNEL_COLLECTION_NAME)
+
+
+def _text_to_json(file_path: str):
+    loader = {
+        "txt": TextLoader,
+    }.get(file_path.split(".")[-1])
+
+    if loader is None:
+        raise ValueError("Not supported file type")
+
+    f = open(file_path, "r")
+    full_txt = f.read()
+
+    json_file = []
+    for txt in full_txt.split('\n#'):
+        t = [i for i in txt.split('\n') if i != '']
+        if len(t) <= 1:
+            continue
+        json_file.append({
+            "Title": t[0],
+            "Description": ''.join(t[1:])
+        })
+
+    res = json.dumps(json_file, ensure_ascii=False)
+    return json.loads(res)
+
+
+def upload_kakao_social_data():
+    print("카카오소셜 chroma에 업로드...")
+    file_path = './data/카카오소셜.txt'
+    data = _text_to_json(file_path=file_path)
+    _upload(kakao_social_collection, data)
+
+
+def upload_kakao_sink_data():
+    print("카카오싱크 chroma에 업로드...")
+    file_path = './data/카카오싱크.txt'
+    data = _text_to_json(file_path=file_path)
+    _upload(kakao_sink_collection, data)
+
+
+def upload_kakaotalk_channel_data():
+    print("카카오톡 채널 chroma에 업로드...")
+    file_path = './data/카카오톡채널.txt'
+    data = _text_to_json(file_path=file_path)
+    _upload(kakaotalk_channel_collection, data)
+
+
+def _upload(collection: Collection, data: json):
+    # 데이터 준비
+    ids = []  # 인덱스
+    documents = []  # 벡터로 변환 저장할 텍스트 데이터로 ChromaDB에 Embedding 데이터가 없으면 자동으로 벡터로 변환해서 저장
+
+    for item in data:
+        id = item['Title'].replace(' ', '-')
+        document = f"{item['Title'].strip()} : {item['Description'].strip()}"
+
+        ids.append(id)
+        documents.append(document)
+
+    collection.add(
+        documents=documents,
+        ids=ids
+    )
+
+
+def query_on_kakao_social(query: str) -> list[str]:
+    return _query_db(kakao_social_collection, query)
+
+
+def query_on_kakao_sink(query: str) -> list[str]:
+    return _query_db(kakao_sink_collection, query)
+
+
+def query_on_kakaotalk_channel(query: str) -> list[str]:
+    return _query_db(kakaotalk_channel_collection, query)
+
+
+def _query_db(collection: Collection, query: str) -> list[str]:
+    docs = collection.query(  # collection.similarity_search(query)
+        query_texts=[query],
+        n_results=3,
+    )
+    return docs["documents"][0]
